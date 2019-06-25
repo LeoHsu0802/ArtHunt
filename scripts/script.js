3d3fd615aa819b1f3163948eb8bbb85976890fa7
Leo Hsu <38006639+LeoHsu0802@users.noreply.github.com>
2019-06-25 13:16:47 +0800

    for PUSH
    add bid

--- scripts/script.js
+++ scripts/script.js
@@ -6,20 +6,25 @@ $('[data-countdown]').each(function() {
       $this.html(event.strftime('剩下 %D 天 %H 時 %M 分 %S 秒'));
     });
   });
-
+//art-box陰影
 $(".art-box").mouseover(function(){
   $(this).css('box-shadow', '9px 9px 7px #888');
 });
 $(".art-box").mouseout(function(){
   $(this).css('box-shadow', 'none');
 });
+//art-bid-btn 變暗
+$(".art-bid-btn").mouseover(function(){
+  $(this).css('background', 'rgb(219, 117, 0)');
+});
+$(".art-bid-btn").mouseout(function(){
+  $(this).css('background', 'rgb(255, 153, 0)');
+});
 
-var bla = $('#bid-price').val();
-$('#new-bid-price').val(bla);
-
-
-
-
+$(document).on("click","#bid-btn",function() {
+  var bid = $(this).prev().prev("#bid-price").val();
+  $(this).parent().parent().parent().find("#new-bid-price").html(bid);
+});
 
 
 })
